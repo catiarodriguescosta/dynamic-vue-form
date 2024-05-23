7e4b114dd65acaf55b74c47c35f8fc1dd2724c60
Catia Costa <catia.rodrigues.costa@gmail.com>
2024-05-23 13:02:47 +0100

    Add LocaleChanger dropdown.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import LocaleChanger from './components/LocaleChanger.vue';
+
 </script>
 
 <template>
+  <h1>{{ $t('message.hello') }}</h1>
   <header>
     <RouterLink to="/">
       <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="20" height="20" />
     </RouterLink>
-    <h1 class="text-3xl font-bold text-green-500 underline">Service Form</h1>  
+    <h1 class="text-3xl font-bold text-green-500 underline">Service Form</h1>
+    <LocaleChanger />
   </header>
   <RouterView />
 </template>
